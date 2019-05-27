196ee63493d1085466644548b471a0ecfad248e7
Jussaraoliveiraa <50372303+Jussaraoliveiraa@users.noreply.github.com>
2019-05-27 07:58:49 -0300

    Add files via upload

---
+++ jogohtml/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1558954643,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/baixo-sheet0.png",
+		"images/baixo-sheet1.png",
+		"images/cima-sheet0.png",
+		"images/cima-sheet1.png",
+		"images/esquerda-sheet0.png",
+		"images/esquerda-sheet1.png",
+		"images/direita-sheet0.png",
+		"images/direita-sheet1.png",
+		"images/background-sheet0.png",
+		"images/enemys-sheet0.png",
+		"images/destroy-sheet0.png"
+	]
+}
